81a30f319251cae6c04da06de5c8058d5f487b51
Mashu02 <94843782+Mashu02@users.noreply.github.com>
2022-06-21 17:37:40 +0200

    Add files via upload
    meer data

--- algo.py
+++ algo.py
@@ -1,26 +1,26 @@
 import pandas as pd
 from math import sqrt
 
-df = pd.read_csv('data_full_full.csv', sep = ';')
+df = pd.read_csv('data_full_full.csv', sep=';')
 
-#print(df.to_string())
+# print(df.to_string())
 
 df_list = df.values.tolist()
-#print(df_list)
+# print(df_list)
 
 
-list_user = [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#https://en.wikipedia.org/wiki/Cosine_similarity
+list_user = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, ]
+
+# https://en.wikipedia.org/wiki/Cosine_similarity
 def cosine_sim(lijst):
-
-    for x in (df_list):
+    for x in df_list:
         sum = 0
         sumA = 0
         sumB = 0
-        for i,j in zip(x, lijst):
-            sum += i*j
-            sumA += i*i
-            sumB += j*j
+        for i, j in zip(x, lijst):
+            sum += i * j
+            sumA += i * i
+            sumB += j * j
 
         cossim = sum / ((sqrt(sumA)) * (sqrt(sumB)))
         print(cossim)
@@ -29,25 +29,4 @@ def cosine_sim(lijst):
 cosine_sim(list_user)
 
 
-def jaccard():
-    intersec = []
-    for x in (df_list):
-        for i,j in zip(x,list_user):
-            print(list_user)
-
-    union = (len(x) + len(list_user)) - intersec
-    y = float(intersec) / union
-
-
-
-#jaccard()
-
-
-#kijk hoeveel 1 en er in de lijst zitten
-#moet dubble for loop en dan kijken of ze allbei gelijk zijn aan 1 dan counter +=1
-#dan counter / len
-
-
-
-#als imput misschien nieuwe lijst
-#en dan top 10 dichts bij de cosine similarity recommenden?
+# verschillende returns en stuk gui
