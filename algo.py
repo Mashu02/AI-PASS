30972e48a9599d6b6d5a8b318dd3ecc6a3546155
Mashu02 <94843782+Mashu02@users.noreply.github.com>
2022-06-22 02:17:58 +0200

    Add files via upload
    heb extra func gemaakt voor bij het feedback scherm

--- algo.py
+++ algo.py
@@ -1,18 +1,33 @@
 import pandas as pd
 from math import sqrt
+from collections import Counter
 
 df = pd.read_csv('data_full_full.csv', sep=';')
-
-# print(df.to_string())
-
 df_list = df.values.tolist()
-# print(df_list)
 
 
-list_user = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, ]
+
+#wil ik doen bij de top 5 combinaties hoevaak die voor komt
+def most_picks():
+    c = Counter()
+    for combination in df_list:
+        c[str(combination)] += 1
+
+
+    #print 13 komt 13 keer voor
+    print(c['[1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]'])
+    #print(c)
+
+
+
+
+
+list_user = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
 
 # https://en.wikipedia.org/wiki/Cosine_similarity
 def cosine_sim(lijst):
+    top = []
+    combination_with_cosim = {}
     for x in df_list:
         sum = 0
         sumA = 0
@@ -21,12 +36,18 @@ def cosine_sim(lijst):
             sum += i * j
             sumA += i * i
             sumB += j * j
-
         cossim = sum / ((sqrt(sumA)) * (sqrt(sumB)))
-        print(cossim)
-        print(x)
+        combination_with_cosim[str(x)] = cossim
+    print(combination_with_cosim)
 
-cosine_sim(list_user)
+#cosine_sim(list_user)
+
+
+#kan nu gewoon de hoogste waarde doen en dan combination in lijst
+
+
+
+
 
 
 # verschillende returns en stuk gui
