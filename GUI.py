3536b5095dea6bcd742d5432ce3e4910a84c3443
Mashu02 <94843782+Mashu02@users.noreply.github.com>
2022-06-23 16:42:31 +0200

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -21,6 +21,7 @@ clicked_button = {"Black" : 0,  "Grey" : 0,  "Silver" : 0,  "White" : 0,  "Brown
                   "Green" : 0,  "Turqoise" : 0,  "Teal" : 0,  "Blue" : 0,  "Violet" : 0,
                   "Purple" : 0,  "Pink" : 0}
 clicked_button_list = []
+clicked_button_list_codes = []
 
 def place_button(button, button_color):
     return pygame.draw.rect(screen, button, button_color)
@@ -47,140 +48,160 @@ def main_menu():
                 feedback()
         elif b.button_black.collidepoint((mouse)):
             if click:
-                if clicked_button["Black"] == 1 or "Black" in clicked_button_list:
+                if clicked_button["Black"] == 1 or (0, 0, 0) in clicked_button_list_codes or "Black" in clicked_button_list:
                     clicked_button["Black"] = 0
+                    clicked_button_list_codes.remove((0, 0, 0))
                     clicked_button_list.remove("Black")
                 else:
                     clicked_button["Black"] = 1
+                    clicked_button_list_codes.append((0, 0, 0))
                     clicked_button_list.append("Black")
         elif b.button_grey.collidepoint((mouse)):
             if click:
-                if clicked_button["Grey"] == 1 or "Grey" in clicked_button_list:
+                if clicked_button["Grey"] == 1 or (128,128,128) in clicked_button_list_codes or "Grey" in clicked_button_list:
                     clicked_button["Grey"] = 0
+                    clicked_button_list_codes.remove((128,128,128))
                     clicked_button_list.remove("Grey")
                 else:
                     clicked_button["Grey"] = 1
+                    clicked_button_list_codes.append((128,128,128))
                     clicked_button_list.append("Grey")
         elif b.button_silver.collidepoint((mouse)):
             if click:
-                if clicked_button["Silver"] == 1 or "Silver" in clicked_button_list:
+                if clicked_button["Silver"] == 1 or (192,192,192) in clicked_button_list_codes or "Silver" in clicked_button_list:
                     clicked_button["Silver"] = 0
+                    clicked_button_list_codes.remove((192,192,192))
                     clicked_button_list.remove("Silver")
                 else:
                     clicked_button["Silver"] = 1
+                    clicked_button_list_codes.append((192,192,192))
                     clicked_button_list.append("Silver")
         elif b.button_white.collidepoint((mouse)):
             if click:
-                if clicked_button["White"] == 1 or "White" in clicked_button_list:
+                if clicked_button["White"] == 1 or (255,255,255) in clicked_button_list_codes or "White" in clicked_button_list:
                     clicked_button["White"] = 0
+                    clicked_button_list_codes.remove((255,255,255))
                     clicked_button_list.remove("White")
                 else:
                     clicked_button["White"] = 1
+                    clicked_button_list_codes.append((255,255,255))
                     clicked_button_list.append("White")
         elif b.button_brown.collidepoint((mouse)):
             if click:
-                if clicked_button["Brown"] == 1 or "Brown" in clicked_button_list:
+                if clicked_button["Brown"] == 1 or (139,69,19) in clicked_button_list_codes or "Brown" in clicked_button_list:
                     clicked_button["Brown"] = 0
+                    clicked_button_list_codes.remove((139,69,19))
                     clicked_button_list.remove("Brown")
                 else:
                     clicked_button["Brown"] = 1
+                    clicked_button_list_codes.append((139,69,19))
                     clicked_button_list.append("Brown")
         elif b.button_red.collidepoint((mouse)):
             if click:
-                if clicked_button["Red"] == 1 or "Red" in clicked_button_list:
+                if clicked_button["Red"] == 1 or (255,0,0) in clicked_button_list_codes or "Red" in clicked_button_list:
                     clicked_button["Red"] = 0
+                    clicked_button_list_codes.remove((255,0,0))
                     clicked_button_list.remove("Red")
                 else:
                     clicked_button["Red"] = 1
+                    clicked_button_list_codes.append((255,0,0))
                     clicked_button_list.append("Red")
         elif b.button_orange.collidepoint((mouse)):
             if click:
-                if clicked_button["Orange"] == 1 or "Orange" in clicked_button_list:
+                if clicked_button["Orange"] == 1 or (255,150,0) in clicked_button_list_codes or "Orange" in clicked_button_list:
                     clicked_button["Orange"] = 0
+                    clicked_button_list_codes.remove((255,150,0))
                     clicked_button_list.remove("Orange")
                 else:
                     clicked_button["Orange"] = 1
+                    clicked_button_list_codes.append((255,150,0))
                     clicked_button_list.append("Orange")
         elif b.button_gold.collidepoint((mouse)):
             if click:
-                if clicked_button["Gold"] == 1 or "Gold" in clicked_button_list:
+                if clicked_button["Gold"] == 1 or (255,215,0) in clicked_button_list_codes or "Gold" in clicked_button_list:
                     clicked_button["Gold"] = 0
+                    clicked_button_list_codes.remove((255,215,0))
                     clicked_button_list.remove("Gold")
                 else:
                     clicked_button["Gold"] = 1
+                    clicked_button_list_codes.append((255,215,0))
                     clicked_button_list.append("Gold")
         elif b.button_beige.collidepoint((mouse)):
             if click:
-                if clicked_button["Beige"] == 1 or "Beige" in clicked_button_list:
+                if clicked_button["Beige"] == 1 or (245,245,220) in clicked_button_list_codes or "Beige" in clicked_button_list:
                     clicked_button["Beige"] = 0
+                    clicked_button_list_codes.remove((245,245,220))
                     clicked_button_list.remove("Beige")
                 else:
                     clicked_button["Beige"] = 1
+                    clicked_button_list_codes.append((245,245,220))
                     clicked_button_list.append("Beige")
         elif b.button_yellow.collidepoint((mouse)):
             if click:
-                if clicked_button["Yellow"] == 1 or "Yellow" in clicked_button_list:
+                if clicked_button["Yellow"] == 1 or (0, 0, 0) in clicked_button_list_codes or "Yellow" in clicked_button_list:
                     clicked_button["Yellow"] = 0
+                    clicked_button_list_codes.remove((0, 0, 0))
                     clicked_button_list.remove("Yellow")
                 else:
                     clicked_button["Yellow"] = 1
+                    clicked_button_list_codes.append((0, 0, 0))
                     clicked_button_list.append("Yellow")
         elif b.button_green.collidepoint((mouse)):
             if click:
-                if clicked_button["Green"] == 1 or "Green" in clicked_button_list:
+                if clicked_button["Green"] == 1 or (0,128,0) in clicked_button_list:
                     clicked_button["Green"] = 0
-                    clicked_button_list.remove("Green")
+                    clicked_button_list.remove((0,128,0))
                 else:
                     clicked_button["Green"] = 1
-                    clicked_button_list.append("Green")
+                    clicked_button_list.append((0,128,0))
         elif b.button_turqoise.collidepoint((mouse)):
             if click:
-                if clicked_button["Turqoise"] == 1 or "Turqoise" in clicked_button_list:
+                if clicked_button["Turqoise"] == 1 or (64,224,208) in clicked_button_list:
                     clicked_button["Turqoise"] = 0
-                    clicked_button_list.remove("Turqoise")
+                    clicked_button_list.remove((64,224,208))
                 else:
                     clicked_button["Turqoise"] = 1
-                    clicked_button_list.append("Turqoise")
+                    clicked_button_list.append((64,224,208))
         elif b.button_teal.collidepoint((mouse)):
             if click:
-                if clicked_button["Teal"] == 1 or "Teal" in clicked_button_list:
+                if clicked_button["Teal"] == 1 or (0,128,128) in clicked_button_list:
                     clicked_button["Teal"] = 0
-                    clicked_button_list.remove("Teal")
+                    clicked_button_list.remove((0,128,128))
                 else:
                     clicked_button["Teal"] = 1
-                    clicked_button_list.append("Teal")
+                    clicked_button_list.append((0,128,128))
         elif b.button_blue.collidepoint((mouse)):
             if click:
-                if clicked_button["Blue"] == 1 or "Blue" in clicked_button_list:
+                if clicked_button["Blue"] == 1 or (0,0,255) in clicked_button_list:
                     clicked_button["Blue"] = 0
-                    clicked_button_list.remove("Blue")
+                    clicked_button_list.remove((0,0,255))
                 else:
                     clicked_button["Blue"] = 1
-                    clicked_button_list.append("Blue")
+                    clicked_button_list.append((0,0,255))
         elif b.button_violet.collidepoint((mouse)):
             if click:
-                if clicked_button["Violet"] == 1 or "Violet" in clicked_button_list:
+                if clicked_button["Violet"] == 1 or (238,130,238) in clicked_button_list:
                     clicked_button["Violet"] = 0
-                    clicked_button_list.remove("Violet")
+                    clicked_button_list.remove((238,130,238))
                 else:
                     clicked_button["Violet"] = 1
-                    clicked_button_list.append("Violet")
+                    clicked_button_list.append((238,130,238))
         elif b.button_purple.collidepoint((mouse)):
             if click:
-                if clicked_button["Purple"] == 1 or "Purple" in clicked_button_list:
+                if clicked_button["Purple"] == 1 or (128,0,128) in clicked_button_list:
                     clicked_button["Purple"] = 0
-                    clicked_button_list.remove("Purple")
+                    clicked_button_list.remove((128,0,128))
                 else:
                     clicked_button["Purple"] = 1
-                    clicked_button_list.append("Purple")
+                    clicked_button_list.append((128,0,128))
         elif b.button_pink.collidepoint((mouse)):
             if click:
-                if clicked_button["Pink"] == 1 or "Pink" in clicked_button_list:
+                if clicked_button["Pink"] == 1 or (255,105,180) in clicked_button_list:
                     clicked_button["Pink"] = 0
-                    clicked_button_list.remove("Pink")
+                    clicked_button_list.remove((255,105,180))
                 else:
                     clicked_button["Pink"] = 1
-                    clicked_button_list.append("Pink")
+                    clicked_button_list.append((255,105,180))
 
 
         pygame.draw.rect(screen, (211, 211, 211), button_feedback)
@@ -204,6 +225,8 @@ def main_menu():
         place_button(c.purple, b.button_purple)
         place_button(c.pink, b.button_pink)
 
+
+        print(clicked_button_list)
 
         for single_color in clicked_button_list:
             draw_text(single_color, font, c.black, screen, 650, y_pos)
--- algo.py
+++ algo.py
@@ -1,11 +1,32 @@
 import pandas as pd
 from math import sqrt
 from collections import Counter
+import numpy as np
+from PIL import Image
+
+
+
 
 df = pd.read_csv('data_full_full.csv', sep=';')
 df_list = df.values.tolist()
 
 list_user = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+
+def color_random_image(list_color_rgb):
+    how_many_colors = len(list_color_rgb)
+
+    # Make a Numpy array 500x500 of random integers 0, 1 or 2
+    na = np.random.randint(0, how_many_colors, (500,500), np.uint8)
+
+    # Convert to PIL Image
+    im = Image.fromarray(na)
+
+    # Push in 3-entry palette with red, blue and yellow:
+    im.putpalette(list_color_rgb)
+
+    # Save
+    im.save('result.png')
+
 
 #wil ik doen bij de top 5 combinaties hoevaak die voor komt
 def most_picks(combination_nummer):
